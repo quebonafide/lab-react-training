3f44f093d69b6339c19efa83421770b8cc895c80
Michal Heimrath <mheimrath1994@gmail.com>
2021-02-23 20:11:30 +0100

    done iteration 9

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 export default class  extends Component {
     state = {
+        counter: 0,
         clicked: false,
         noGlasses: '/img/persons/maxence.png',
         glasses: '/img/persons/maxence-glasses.png'
     }
+
+    clickHandler = () => {
+        console.log('click');
+            this.setState((state, props) => ({
+            counter: state.counter + 1
+            }))
+      }
 
       clickHandler = () => {
         this.setState((state, props) => ({
